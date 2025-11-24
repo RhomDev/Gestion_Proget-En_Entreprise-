058d3b10fb7a331a76891e961417df831deeef0f
RigonAj <rigonajvazi@proton.me>
2025-11-24 15:10:37 +0100

    test

--- programme/main.py
+++ programme/main.py
@@ -1,2 +1,9 @@
 import pygame
 
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
+
+
+print("Hello World")
---
+++ programme/ouvrier.py
@@ -0,0 +1,8 @@
+class Ouvrier:
+
+    def __init__(self, nom):
+        self.nom = nom
+
+    self.pose=[0,0]
+    self.state=0
+
