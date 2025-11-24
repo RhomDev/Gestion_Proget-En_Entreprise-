8a964b7e1946a19db501ba87e6d643597c73771f
RhomDev <pradeauromaric@gmail.com>
2025-11-24 15:08:32 +0100

    initial programme

---
+++ programme/buildozer.spec
@@ -0,0 +1,12 @@
+[app]
+title = En Entreprise
+package.name = enentreprise
+package.domain = org.test
+source.dir = .
+source.include_exts = py,png,jpg,mp3,ttf
+version = 0.1
+requirements = python3,kivy,pygame_sdl2
+android.api = 30
+android.minapi = 21
+android.ndk = 21d
+android.permissions = INTERNET
--- programme/main.py
+++ programme/main.py
@@ -1,2 +1,29 @@
 import pygame
 
+# Initialisation de Pygame
+pygame.init()
+
+# Dimensions de la fenêtre
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Mon Jeu Pygame pour Android")
+
+# Couleurs
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+# Position initiale du carré
+x, y = WIDTH // 2, HEIGHT // 2
+speed = 5
+
+# Boucle principale
+running = True
+clock = pygame.time.Clock()
+
+while running:
+    print(pygame.key.get_pressed())
+    for event in pygame.event.get():
+        print(pygame.key.get_pressed())
+
+pygame.quit()
+
