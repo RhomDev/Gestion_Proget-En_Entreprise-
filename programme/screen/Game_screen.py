89804a0fdb907531575bef0dc115d9f5400d5be2
RigonAj <rigonajvazi@proton.me>
2025-12-14 19:15:22 +0100

    Création dossier map

--- programme/screen/Game_screen.py
+++ programme/screen/Game_screen.py
@@ -1,12 +1,24 @@
 import pygame
+
 from programme.Object import *
-from programme.utils.ouvrier import *
 from programme.utils.map import *
+from programme.utils.ouvrier import *
+
 
 def game_screen_init(screen):
-    global panel_outil, tache_bouton, deplacement_bouton, mission_bouton, hint_panel, var_open_panel, bob, mapes
+    global \
+        panel_outil, \
+        tache_bouton, \
+        deplacement_bouton, \
+        mission_bouton, \
+        hint_panel, \
+        var_open_panel, \
+        bob, \
+        mapes
     var_open_panel = True
-    img_background_outil = pygame.image.load("programme/src/img/game_img/background_btn_option.jpg")
+    img_background_outil = pygame.image.load(
+        "programme/src/img/game_img/background_btn_option.jpg"
+    )
     img_bouton_standard = pygame.image.load("programme/src/img/util/btn_standard.png")
     img_hint_panel = pygame.image.load("programme/src/img/game_img/hint_panel.png")
 
@@ -15,23 +27,48 @@ def game_screen_init(screen):
 
     align_left = 70
 
-    panel_outil = Rectangle(screen, (0, screen.get_height() - 140), dim=(screen.get_width(), 140),
-                            img=img_background_outil, scale=1)
+    panel_outil = Rectangle(
+        screen,
+        (0, screen.get_height() - 140),
+        dim=(screen.get_width(), 140),
+        img=img_background_outil,
+        scale=1,
+    )
 
-    tache_bouton = Button(screen, (align_left, screen.get_height() - 110), img_bouton_standard, 4, text="Tache",
-                          color_input='Black', color_input1='White')
-    deplacement_bouton = Button(screen, (align_left + 230, screen.get_height() - 110), img_bouton_standard, 4,
-                                text="Deplacement",
-                                color_input='Black', color_input1='White')
-    mission_bouton = Button(screen, (align_left + 230 * 2, screen.get_height() - 110), img_bouton_standard, 4,
-                            text="Mission",
-                            color_input='Black', color_input1='White')
+    tache_bouton = Button(
+        screen,
+        (align_left, screen.get_height() - 110),
+        img_bouton_standard,
+        4,
+        text="Tache",
+        color_input="Black",
+        color_input1="White",
+    )
+    deplacement_bouton = Button(
+        screen,
+        (align_left + 230, screen.get_height() - 110),
+        img_bouton_standard,
+        4,
+        text="Deplacement",
+        color_input="Black",
+        color_input1="White",
+    )
+    mission_bouton = Button(
+        screen,
+        (align_left + 230 * 2, screen.get_height() - 110),
+        img_bouton_standard,
+        4,
+        text="Mission",
+        color_input="Black",
+        color_input1="White",
+    )
 
-    hint_panel = Button(screen, (screen.get_width() - 80, screen.get_height() - 170), img_hint_panel, 1)
+    hint_panel = Button(
+        screen, (screen.get_width() - 80, screen.get_height() - 170), img_hint_panel, 1
+    )
 
-    
-    mapes = Map(screen,1)
-    bob = Ouvrier(screen,"C",mapes)
+    mapes = Map(screen, 1)
+    bob = Ouvrier(screen, "D", mapes)
 
 
 def game_update():
@@ -39,7 +76,7 @@ def game_update():
     panel_outil.update()
     hint_panel.update()
     bob.update()
-    
+
     if var_open_panel:
         tache_bouton.update()
         deplacement_bouton.update()
@@ -71,7 +108,9 @@ def open_panel():
     img_button_hint_panel = pygame.transform.rotate(hint_panel.get_image(), 180)
     hint_panel.change_image(img_button_hint_panel)
     # Déplacer le bouton (exemple : vers le haut)
-    hint_panel.change_position((hint_panel.get_position()[0], screen.get_height() - 170))
+    hint_panel.change_position(
+        (hint_panel.get_position()[0], screen.get_height() - 170)
+    )
 
     # Changer la taille et la position du panel
     panel_outil.change_dim((panel_outil.get_rect().width, 140))
@@ -84,7 +123,9 @@ def event_outil_panel(event):
         tache_bouton.animation_check_color(pygame.mouse.get_pos())
         tache_bouton.event(event, pygame.mouse.get_pos(), lambda: print("tache"))
         deplacement_bouton.animation_check_color(pygame.mouse.get_pos())
-        deplacement_bouton.event(event, pygame.mouse.get_pos(), lambda: print("deplacement"))
+        deplacement_bouton.event(
+            event, pygame.mouse.get_pos(), lambda: print("deplacement")
+        )
         mission_bouton.animation_check_color(pygame.mouse.get_pos())
         mission_bouton.event(event, pygame.mouse.get_pos(), lambda: print("mission"))
     else:
@@ -94,7 +135,7 @@ def event_outil_panel(event):
 def Game_screen(screen, pageset, pageget, clock):
     game_active = True
     game_screen_init(screen)
-    while (game_active):
+    while game_active:
         screen.fill((35, 206, 235))
         game_update()
 
--- programme/utils/map.py
+++ programme/utils/map.py
@@ -1,59 +1,59 @@
-import pygame
 from math import sqrt
 
-def dist(x,y,x1,y1):
-    return sqrt((x-x1)**2 + (y-y1)**2)
+import pygame
+
+
+def dist(x, y, x1, y1):
+    return sqrt((x - x1) ** 2 + (y - y1) ** 2)
+
 
 class Map:
-    def __init__(s,screen,choix):
-        s.screen=screen
-        s.resolution = (s.screen.get_width(),s.screen.get_height())
-        s.image = pygame.image.load(f"programme/src/img/game_img/map{choix}.png")
-        s.image = pygame.transform.scale(s.image,s.resolution)
+    def __init__(s, screen, choix):
+        s.screen = screen
+        s.resolution = (s.screen.get_width(), s.screen.get_height())
+        s.image = pygame.image.load(f"programme/src/img/map/map{choix}.png")
+        s.image = pygame.transform.scale(s.image, s.resolution)
         s.nodes = Nodes()
 
     def Draw(s):
-        s.screen.blit(s.image, (0,0))
+        s.screen.blit(s.image, (0, 0))
 
-    def Set_Map(s,choix):
-        s.image = pygame.image.load(f"programme/src/img/game_img/map{choix}.png")
-        s.image = pygame.transform.scale(s.image,s.resolution)
-
+    def Set_Map(s, choix):
+        s.image = pygame.image.load(f"programme/src/img/map/map{choix}.png")
+        s.image = pygame.transform.scale(s.image, s.resolution)
 
     def update(s):
         s.Draw()
 
+
 class Node:
-    def __init__(s,data,name):
+    def __init__(s, data, name):
         s.name = name
         s.data = data
         s.next = []
-    def nexte(s,obj):
-        min= 1000000
-        k=None
+
+    def nexte(s, obj):
+        min = 1000000
+        k = None
         for i in range(len(s.next)):
             nod = s.next[i]
-            x,y = nod.data[0],nod.data[1]
-            distance = dist(x,y,obj[0],obj[1])
-            print(distance, nod.name , nod.data , obj)
+            x, y = nod.data[0], nod.data[1]
+            distance = dist(x, y, obj[0], obj[1])
+            print(distance, nod.name, nod.data, obj)
             if obj[0] == x and obj[1] == y:
                 return s.next[i]
-            if distance < min :
-                k=i
+            if distance < min:
+                k = i
         return s.next[k]
+
 
 class Nodes:
     def __init__(s):
-        s.noeuds = {
-        "A": (167 , 159),
-        "B": (167 , 292),
-        "C": (354 , 292),
-        "D": (357 , 244)
-        }
-        s.a = Node((167 , 159),"A")
-        s.b = Node((167 , 292),"B")
-        s.c = Node((354 , 292),"C")
-        s.d = Node((357 , 244),"D")
+        s.noeuds = {"A": (167, 159), "B": (167, 292), "C": (354, 292), "D": (357, 244)}
+        s.a = Node((167, 159), "A")
+        s.b = Node((167, 292), "B")
+        s.c = Node((354, 292), "C")
+        s.d = Node((357, 244), "D")
 
         s.a.next.append(s.b)
         s.b.next.append(s.a)
